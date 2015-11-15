07815f9945adb7b0080bf2e964ef42b92e8b9fa7
ACamposTams <acampostams@gmail.com>
2015-11-14 20:06:31 -0600

    Added devise/login/user-event relation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,11 +9,11 @@ class EventsController < ApplicationController
 	end
 
 	def new
-		@event = Event.new
+		@event = current_user.events.build
 	end
 
 	def create
-		@event = Event.new(ev_params)
+		@event = current_user.events.build(ev_params)
 
 		if @event.save
 			redirect_to @event, notice: "Succesfully created new event"
